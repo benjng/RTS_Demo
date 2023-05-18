5d2d5e2c88be267bfeeb9f61f8ab40ac402e7628
benjng <ppbenng@gmail.com>
2023-05-18 23:07:57 +0900

    Updating grid test

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -8,6 +8,7 @@ public class Grid {
     private int height;
     private float cellSize;
     private int[,] gridArray;
+    private TextMesh[,] debugTextArray;
 
     public Grid(int width, int height, float cellSize) {
         this.width = width;
@@ -15,16 +16,19 @@ public class Grid {
         this.cellSize = cellSize;
 
         gridArray = new int[width, height];
+        debugTextArray = new TextMesh[width, height];
 
         for (int x = 0; x < gridArray.GetLength(0); x++){
             for (int z = 0; z < gridArray.GetLength(1); z++){
-                CreateWorldText(gridArray[x, z].ToString(), null, GetWorldPosition(x, z), 20, Color.white, TextAnchor.MiddleCenter);
+                debugTextArray[x, z] = CreateWorldText(gridArray[x, z].ToString(), null, GetWorldPosition(x, z) + new Vector3(cellSize, 0, cellSize) * .5f, 20, Color.white, TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x + 1, z), Color.white, 100f);
             }
         }
-    }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
 
-    private Vector3 GetWorldPosition(int x, int z){
-        return new Vector3(x, 0, z) * cellSize;
+        // SetValue(2, 1, 56);
     }
 
     public static TextMesh CreateWorldText(string text, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 40, Color? color = null, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = 5000){
@@ -37,6 +41,7 @@ public class Grid {
         Transform transform = gameObject.transform;
         transform.SetParent(parent, false);
         transform.localPosition = localPosition;
+        transform.rotation = Quaternion.Euler(90f, 0, 0);
         TextMesh textMesh = gameObject.GetComponent<TextMesh>();
         textMesh.anchor = textAnchor;
         textMesh.alignment = textAlignment;
@@ -45,5 +50,27 @@ public class Grid {
         textMesh.color = color;
         textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
         return textMesh;
+    }
+
+    private Vector3 GetWorldPosition(int x, int z){
+        return new Vector3(x, 0, z) * cellSize;
+    }
+
+    private void GetXZ(Vector3 worldPosition, out int x, out int z){
+        x = Mathf.FloorToInt(worldPosition.x/cellSize);
+        z = Mathf.FloorToInt(worldPosition.z/cellSize);
+    }
+
+    public void SetValue(int x, int z, int value){
+        if (x >= 0 && z >= 0 && x < width && z < height){
+            gridArray[x, z] = value;
+            debugTextArray[x, z].text = gridArray[x, z].ToString();
+        } 
+    }
+
+    public void SetValue(Vector3 worldPosition, int value){
+        int x, z;
+        GetXZ(worldPosition, out x, out z);
+        SetValue(x, z, value);
     }
 }
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    private Grid grid;
+
     void Start()
     {
-        Grid grid = new Grid(4, 2, 10f);
+        grid = new Grid(4, 2, 10f); 
     }
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)){
+            grid.SetValue(GetMouseWorldPosition(), 56);
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition(){
+        Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        vec.y = 0f;
+        return vec;
     }
 }
