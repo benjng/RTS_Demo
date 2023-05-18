f72eea2e92320c58264b4f90ba0eb6eae9ee1a4f
benjng <ppbenng@gmail.com>
2023-05-19 02:35:30 +0900

    Update get grid, origin changable

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -6,28 +6,35 @@ public class Testing : MonoBehaviour
 {
     private Grid grid;
     Vector3 vec;
+    Ray ray;
 
     void Start()
     {
-        grid = new Grid(4, 2, 10f); 
+        grid = new Grid(4, 2, 2f, new Vector3(20, 0, 0)); 
     }
     void Update()
     {
         if (Input.GetMouseButtonDown(0)){
             grid.SetValue(GetMouseWorldPosition(), 56);
         }
+        if (Input.GetMouseButtonDown(1)){
+            Debug.Log(grid.GetValue(GetMouseWorldPosition()));
+        }
     }
 
     private Vector3 GetMouseWorldPosition(){
         RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, out hit, Mathf.Infinity);
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out hit, 1000f);
         vec = hit.point;
-        vec.y = 0f;
+        // vec.y = 0f;
         return vec;
     }
 
     private void OnDrawGizmos() {
-        Gizmos.DrawSphere(vec, 2f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(vec, 0.2f);
+
+        Gizmos.DrawLine(ray.origin, ray.direction*1000f);
     }
 }
