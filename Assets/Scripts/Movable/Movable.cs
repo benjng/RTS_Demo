fbcdfe3c7f5ccd0a167d574839344fe9ba2defd9
benjng <ppbenng@gmail.com>
2023-06-08 12:27:24 +0900

    adding enemy

---
+++ Assets/Scripts/Movable/Movable.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Movable : MonoBehaviour
+{
+    public int CurrentHP;
+    public UnitSO unitSO;
+
+    void Start(){
+
+    }
+}
