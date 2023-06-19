6a5154f6b76791d4c78a9ae610d3d668b665cc91
benjng <ppbenng@gmail.com>
2023-06-19 23:27:15 +0900

    Refactor

--- Assets/Scripts/Movable/EnemyMovement.cs
+++ Assets/Scripts/Movable/EnemyMovement.cs
@@ -16,15 +16,6 @@ public class EnemyMovement : UnitMovement
         // Prevent automovement when precise/atk moving
         if(myAgent.velocity.magnitude != 0) return;
 
-        // Make sure target is still alive
-        if (targetDetector.targetList.Count == 0) return;
-        if (targetDetector.targetList.First.Value == null) {
-            targetDetector.targetList.RemoveFirst(); // remove empty
-            return;
-        }
-
-        // Automvmt by targetList (Chase target when there is any)
-        destination = GetDestinationByTarget(targetDetector.targetList.First.Value);
-        MoveToPos(destination);
+        AutoMovement();
     }
 }
--- Assets/Scripts/Movable/Units/UnitMovement.cs
+++ Assets/Scripts/Movable/Units/UnitMovement.cs
@@ -34,19 +34,11 @@ public class UnitMovement : MonoBehaviour
             myAgent.ResetPath();
         }
 
+        // TODO: override Automovement when Manualmovement is in progress
         // Prevent automovement when precise/atk moving
         if(myAgent.velocity.magnitude != 0) return;
 
-        // Make sure target is still alive
-        if (targetDetector.targetList.Count == 0) return;
-        if (targetDetector.targetList.First.Value == null) {
-            targetDetector.targetList.RemoveFirst();
-            return;
-        }
-
-        // Automvmt by targetList (Chase target when there is any)
-        destination = GetDestinationByTarget(targetDetector.targetList.First.Value);
-        MoveToPos(destination);
+        AutoMovement();
     }
 
     void ManualMovement(){
@@ -76,6 +68,19 @@ public class UnitMovement : MonoBehaviour
         // Move till reaching destination without being stopped
         if (destination != null)
             MoveToPos(destination); 
+    }
+
+    protected void AutoMovement(){
+        // Make sure target is still alive
+        if (targetDetector.targetList.Count == 0) return;
+        if (targetDetector.targetList.First.Value == null) {
+            targetDetector.targetList.RemoveFirst();
+            return;
+        }
+
+        // Automvmt by targetList (Chase target when there is any)
+        destination = GetDestinationByTarget(targetDetector.targetList.First.Value);
+        MoveToPos(destination);
     }
 
     void UpdateTarget(RaycastHit hit){
